86cecdd3801684a486b29405379977095a641d02
larih <mewmewdevart@gmail.com>
2024-07-13 20:19:46 -0300

    chore: add the eslint + prettier + json settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import './App.css'
+import './App.css';
 
 function App() {
-
   return (
     <>
-      <section className='bg-red-500 w-full h-[100vh]'>
+      <section className="bg-red-500 w-full h-[100vh]">
         <h1>Hello World!</h1>
       </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
