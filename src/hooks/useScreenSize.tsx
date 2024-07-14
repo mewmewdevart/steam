512c83f086b41fff2b09b22bb75439ffbba9784c
larih <mewmewdevart@gmail.com>
2024-07-14 19:07:30 -0300

    feat: add the useScreen hook

---
+++ src/hooks/useScreenSize.tsx
@@ -0,0 +1,20 @@
+import { useState, useEffect } from 'react';
+
+const useScreenSize = () => {
+  const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth <= 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth <= 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return isSmallScreen;
+};
+
+export default useScreenSize;
