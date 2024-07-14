b7add0a42f19ed1423ad94a3c0a64fe53fe798df
larih <mewmewdevart@gmail.com>
2024-07-14 19:09:10 -0300

    fix: fix in the tailwind config of styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,60 +9,63 @@ module.exports = {
   ],
   theme: {
     extend: {
+      backgroundImage: {
+        'custom-gradient-10': 'linear-gradient(90deg, #06BFFF 0%, #2D73FF 100%)',
+      },
       colors: {
         brand: {
           primary: {
-            10: "#8ACEFF",
-            20: "#5CBAFF",
-            30: "#2EA7FF",
-            40: "#0094FF",
-            50: "#007CD6",
-            60: "#0065AD",
+            '10': custom.colors.brand.primary['10'].hex,
+            '20': custom.colors.brand.primary['20'].hex,
+            '30': custom.colors.brand.primary['30'].hex,
+            '40': custom.colors.brand.primary['40'].hex,
+            '50': custom.colors.brand.primary['50'].hex,
+            '60': custom.colors.brand.primary['60'].hex,
           },
           secondary: {
-            10: "#AAAAAA",
-            20: "#1B2838",
-            30: "#0E131E",
+            '10': custom.colors.brand.secondary['10'].hex,
+            '20': custom.colors.brand.secondary['20'].hex,
+            '30': custom.colors.brand.secondary['30'].hex,
           },
           tertiary: {
-            10: "#FFFFFF",
+            '10': custom.colors.brand.tertiary['10'].hex,
           },
         },
         support: {
           information: {
-            10: "#E0E0E0",
+            '10': custom.colors.suport.information['10'].hex,
           },
           error: {
-            10: "#F03D3D",
+            '10': custom.colors.suport.error['10'].hex,
           },
           warning: {
-            10: "#FFCE52",
+            '10': custom.colors.suport.warning['10'].hex,
           },
           success: {
-            10: "#0BB07B",
+            '10': custom.colors.suport.sucess['10'].hex,
           },
           message: {
-            10: "#2962FF",
+            '10': custom.colors.suport.message['10'].hex,
           },
         },
       },
-      fontWeight: {
-        light: 300,
-        regular: 400,
-        bold: 700,
-        black: 800,
+      borderRadius: {
+        small: custom.border.small,
+        medium: custom.border.medium,
+        pill: custom.border.pill,
       },
       fontSize: {
-        xlarge: "24px",
-        large: "20px",
-        medium: "18px",
-        small: "16px",
-        xsmall: "14px",
+        xlarge: custom.typography.text.xlarge.fontSize,
+        large: custom.typography.text.large.fontSize,
+        medium: custom.typography.text.medium.fontSize,
+        small: custom.typography.text.small.fontSize,
+        xsmall: custom.typography.text.xsmall.fontSize,
       },
-      borderRadius: {
-        small: "10px",
-        medium: "20px",
-        pill: "1000px",
+      fontWeight: {
+        light: custom.typography.weight.light,
+        regular: custom.typography.weight.regular,
+        bold: custom.typography.weight.bold,
+        black: custom.typography.weight.black,
       },
     },
   },
